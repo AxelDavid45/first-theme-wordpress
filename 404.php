1b080ebcc464451690b17bdbea48640f246db4dd
Axel Espinosa <axeldavid45@gmail.com>
2020-02-15 02:07:23 +0000

    Agregamos nuestra vista para el 404 de una pagina

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<main class="container">
+    <div class="404">
+        <h1>Página no encontrada</h1>
+        <h4>Da <a href="<?= home_url() ?>">clic aqui</a> para ir al inicio</h4>
+    </div>
+</main>
+
+<?php get_footer(); ?>
