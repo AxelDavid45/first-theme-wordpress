ff2cf988c04d9f422985de892e1af3dc15432f7a
Axel Espinosa <axeldavid45@gmail.com>
2020-02-15 17:38:11 +0000

    Agregamos un listado de otros productos para que el usuario pueda seguir navegando

--- single-producto.php
+++ single-producto.php
@@ -3,7 +3,7 @@
         <?php if (have_posts()): ?>
             <?php while (have_posts()): the_post(); ?>
 
-                <div class="card mb-3" >
+                <div class="card mb-3">
                     <div class="row no-gutters">
                         <div class="col-md-6">
                             <?php the_post_thumbnail('large'); ?>
@@ -22,12 +22,49 @@
                                 </button>
                             </div>
                         </div>
+
                     </div>
+                    <div class="row">
+                        <div class="col-12 text-center">
+                            <h3>Productos Relacionados:</h3>
+                        </div>
+                    </div>
+                    <div class="row justify-content-center">
+                        <?php
+                        //Creating the arguments for custom loop
+                        $args = array(
+                            'post_type' => 'producto',
+                            'posts_per_page' => 3,
+                            'order' => 'DESC',
+                            'orderBy' => 'title'
+                        );
+                        //Create the object
+                        $producto = new WP_Query($args);
+                        ?>
+                        <?php if ($producto->have_posts()):
+                            while ($producto->have_posts()):
+                                $producto->the_post(); ?>
+                                <div class="col-md-2  text-center">
+                                    <a href="<?php the_permalink(); ?>">
+                                        <picture>
+                                            <?php the_post_thumbnail('thumbnail'); ?>
+                                        </picture>
+                                        <h5><?php the_title(); ?></h5>
+                                    </a>
+                                </div>
+
+
+                            <?php
+                            endwhile;
+                        endif;
+                        ?>
+                    </div>
+                    <?php
+                    //Adding the navigation part
+                    get_template_part('template-parts/post', 'navigation');
+                    ?>
                 </div>
-            <?php
-            //Adding the navigation part
-                get_template_part('template-parts/post', 'navigation');
-                ?>
+
 
             <?php endwhile; ?>
         <?php endif; ?>
