685782835913979b46cdcb38a1db260a4b5f2777
Axel Espinosa <axeldavid45@gmail.com>
2020-02-29 19:18:20 +0000

    Ahora el single product solo muestra contenido relacionado a las taxonomias a las que se relacione el producto

--- single-producto.php
+++ single-producto.php
@@ -31,12 +31,28 @@
                     </div>
                     <div class="row justify-content-center">
                         <?php
+                        // Getting the taxonomies terms
+                        $taxonomy = get_the_terms(get_the_ID(), 'categoria-productos');
+                        //Array only with slugs
+                        $taxonomyTerms = array();
+                        // Creating an array only with the slugs
+                        foreach ($taxonomy as $t):
+                            array_push($taxonomyTerms, $t->slug);
+                        endforeach;
+
                         //Creating the arguments for custom loop
                         $args = array(
                             'post_type' => 'producto',
                             'posts_per_page' => 3,
                             'order' => 'DESC',
-                            'orderBy' => 'title'
+                            'orderBy' => 'title',
+                            'tax_query' => array(
+                                array(
+                                    'taxonomy' => 'categoria-productos',
+                                    'field' => 'slug',
+                                    'terms' => $taxonomyTerms
+                                )
+                            )
                         );
                         //Create the object
                         $producto = new WP_Query($args);
